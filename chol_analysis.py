2e6b5c68b410d33b79b96b51c6d875e4af8c6793
Hansol Chang <hc270@duke.edu>
2020-01-29 22:47:24 -0500

    Added LDL analysis and modified choletserol_analysis

--- chol_analysis.py
+++ chol_analysis.py
@@ -6,14 +6,24 @@ def HDL_analysis(HDL_level) :
 	else:
 		return "Low"
 
+def LDL_analysis(LDL_level) :
+	if LDL_level <= 130 :
+		return 'Normal'
+	elif 130 < LDL_level <= 159 :
+		return 'Borderline high'
+	elif 160 <= LDL_level < 189	:
+		return 'High'
+	elif 190 <= LDL_level :
+		return 'Very high'
+
 def cholesterol_analysis() :
 	print("Cholesterol_Analysis")
-	HDLinput = input("Enter test result: ")
-	test_info = HDLinput.split("=")
-	if test_info[0] == "HDL" :
-		answer = HDL_analysis(int(test_info[1]))
-		print("The level in {}".format(answer))
-
+	DLinput = input("Enter test result: ")
+	test_info = DLinput.split("=")
+	if test_info[0].strip() == "HDL" :
+		print(HDL_analysis(int(test_info[1])))
+	elif test_info[0].strip() == "LDL" :
+		print(HDL_analysis(int(test_info[1])))
 
 def interface() :
 	while True :
