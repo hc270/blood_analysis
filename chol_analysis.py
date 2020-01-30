d2dbbc6319c6f43c6f51bb4a49829ec970278396
Hansol Chang <hc270@duke.edu>
2020-01-29 21:34:05 -0500

    Built basic outline for interface

---
+++ chol_analysis.py
@@ -0,0 +1,10 @@
+def interface() :
+	print("Cholesterol Calculator")
+	print("options: ")
+	print("  9 - Quit")
+	choice = input("Enter your option: ")
+	if choice == '9' :
+		return 
+
+if  __name__ == "__main__" :
+	interface()
