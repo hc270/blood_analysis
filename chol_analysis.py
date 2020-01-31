07fc83c948b4ca47cbcf63681ec8fe13c3724d4c
Hansol Chang <hc270@duke.edu>
2020-01-31 12:45:44 -0500

    Fixed according to the test result using pep8

--- chol_analysis.py
+++ chol_analysis.py
@@ -1,41 +1,45 @@
-def HDL_analysis(HDL_level) :
-	if HDL_level >= 60 :
-		return "Normal"
-	elif 40 <= HDL_level < 60 :
-		return "Borderline low"
-	else:
-		return "Low"
+def HDL_analysis(HDL_level):
+    if HDL_level >= 60:
+        return "Normal"
+    elif 40 <= HDL_level < 60:
+        return "Borderline low"
+    else:
+        return "Low"
 
-def LDL_analysis(LDL_level) :
-	if LDL_level <= 130 :
-		return 'Normal'
-	elif 130 < LDL_level <= 159 :
-		return 'Borderline high'
-	elif 160 <= LDL_level < 189	:
-		return 'High'
-	elif 190 <= LDL_level :
-		return 'Very high'
 
-def cholesterol_analysis() :
-	print("Cholesterol_Analysis")
-	DLinput = input("Enter test result: ")
-	test_info = DLinput.split("=")
-	if test_info[0].strip() == "HDL" :
-		print(HDL_analysis(int(test_info[1])))
-	elif test_info[0].strip() == "LDL" :
-		print(HDL_analysis(int(test_info[1])))
+def LDL_analysis(LDL_level):
+    if LDL_level <= 130:
+        return 'Normal'
+    elif 130 < LDL_level <= 159:
+        return 'Borderline high'
+    elif 160 <= LDL_level < 189:
+        return 'High'
+    elif 190 <= LDL_level:
+        return 'Very high'
 
-def interface() :
-	while True :
-		print("Cholesterol Calculator")
-		print("options: ")
-		print("  1 - Cholesterol Analysis")
-		print("  9 - Quit")
-		choice = input("Enter your option: ")
-		if choice == '9' :
-			return 
-		elif choice == '1' :
-			cholesterol_analysis()
 
-if  __name__ == "__main__" :
-	interface()
+def cholesterol_analysis():
+    print("Cholesterol_Analysis")
+    DLinput = input("Enter test result: ")
+    test_info = DLinput.split("=")
+    if test_info[0].strip() == "HDL":
+        print(HDL_analysis(int(test_info[1])))
+    elif test_info[0].strip() == "LDL":
+        print(HDL_analysis(int(test_info[1])))
+
+
+def interface():
+    while True:
+        print("Cholesterol Calculator")
+        print("options: ")
+        print("  1 - Cholesterol Analysis")
+        print("  9 - Quit")
+        choice = input("Enter your option: ")
+        if choice == '9':
+            return
+        elif choice == '1':
+            cholesterol_analysis()
+
+
+if __name__ == "__main__":
+    interface()
