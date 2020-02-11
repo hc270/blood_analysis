5c2cbc4f9f4f76552a13fc6d095edf745184032c
Hansol Chang <hc270@duke.edu>
2020-02-11 12:04:42 -0500

    we are testing travis

--- test_chol_analysis.py
+++ test_chol_analysis.py
@@ -1,19 +1,31 @@
-def test_HDL_analysis_normal():
-    from chol_analysis import HDL_analysis
-    answer = HDL_analysis(80)
-    expected = "Normal"
-    assert answer == expected
+import pytest
 
 
-def test_HDL_analysis_bl():
+def test_HDL_analysis():
+    """Parametrize Tests HDL_analysis method
+    Args:
+        HDL_level
+    Returns:
+        Passed if answer -- expected
+    """
+
     from chol_analysis import HDL_analysis
-    answer = HDL_analysis(40)
-    expected = "Borderline low"
+    input = 100
+    answer = HDL_analysis(input)
+    expected = 'Normal'
     assert answer == expected
 
 
 def test_LDL_analysis():
+    """Parametrize Tests HDL_analysis method
+    Args:
+        HDL_level
+    Returns:
+        Passed if answer -- expected
+    """
+
     from chol_analysis import LDL_analysis
-    answer = LDL_analysis(165)
-    expected = "High"
+    input = 100
+    answer = LDL_analysis(input)
+    expected = 'Normal'
     assert answer == expected
