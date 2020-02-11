dccea7d81accb236e4ad4ca7b8257db2393f2bd7
Hansol Chang <hc270@duke.edu>
2020-02-11 12:21:29 -0500

    final testing

--- test_chol_analysis.py
+++ test_chol_analysis.py
@@ -8,7 +8,6 @@ def test_HDL_analysis():
     Returns:
         Passed if answer -- expected
     """
-
     from chol_analysis import HDL_analysis
     input = 100
     answer = HDL_analysis(input)
@@ -23,9 +22,19 @@ def test_LDL_analysis():
     Returns:
         Passed if answer -- expected
     """
-
     from chol_analysis import LDL_analysis
     input = 100
     answer = LDL_analysis(input)
     expected = 'Normal'
+    assert answer == expected
+
+
+@pytest.mark.parametrize("input, output, expected", [
+    (100, 'Normal', True),
+    (50, 'Borderline low', True),
+    (20, 'Low', True),
+])
+def test_HDL_analysis2(input, output, expected):
+    from chol_analysis import HDL_analysis
+    answer = HDL_analysis(input) == output
     assert answer == expected
